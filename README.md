1c8065cf60345eb1361cd4579c6a9850dbc6c0a0
Shane Kelly <G00398368@gmit.ie>
2021-03-24 05:47:56 -0700

    Pushing 'bmi.py' to respoistory

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pands-problem-sheet
 This repository contains all of the problem sheets which we completed through the module 'Programming and Scripting'
+
+The work carried out these problems were first done each week within the 'Programming2021Coursework' repository https://github.com/ShaneKelly89/Programming2021Coursework.git where they can be located each week alongside the labs which were also carried out each week. 
---
+++ bmi.py
@@ -0,0 +1,26 @@
+
+#Bmi.py
+#This program will be used to calculate a person's bmi
+#Author Shane Kelly
+
+#Firstly, I must code for 'weight' and have the user enter their weight 
+#weight = int(input("Enter weight here\n"))
+
+#Second, I must do the same for 'height'
+#height = float(input("Enter Height here\n"))
+
+#I will then need to compute the calculation for bmi 
+
+weight = int(input("Enter weight here\n"))
+height = float(input("Enter Height here in meters\n"))
+bmi = weight / (height * height)
+print ("bmi is {}".format(bmi))   #https://www.w3schools.com/python/python_string_formatting.asp
+
+#Enter weight here first result below (changes made to Height (1.8))
+#65
+#Enter Height here
+#180
+#bmi is 0.002006172839506173
+
+#Trial and error found height is expected to be in meters for this formula 
+#to work. As such, I have changed the user output to ask the user to enter their hight in meters 
